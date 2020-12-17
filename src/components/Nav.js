040eb917346f3647aac1558b7c1377220f055e9e
George Flores <georgeflores886@gmail.com>
2020-12-17 14:59:31 -0500

    initial commit, added nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav className='nav-header'>
+
+            <div className="brand">devjobs</div>
+                <div className="toggle-them">
+                    <label htmlFor='checkbox'>
+                        
+                    </label>
+
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Nav
