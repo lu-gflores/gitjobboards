b11130089862d8838fb6a921221263f24ef54446
George Flores <georgeflores886@gmail.com>
2020-12-18 13:13:48 -0500

    rendered jobs from api, need to adjust loading state

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -8,11 +8,24 @@ const [jobPost, setJobPost] = useState(null);
 const [input, setInput] = useState('');
 
 useEffect(() => {
-    axios.get(``)
-})
+    axios.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json`)
+    .then(data => {
+        setJobPost(data.data)
+        console.log(data.data)
+    })
+    .catch(err => console.log(err))
+}, [])
 
+const searchJobs = () => {
+    axios.get(`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?`)
+    .then(data => {
+        setJobPost(data.data)
+    })
+}
     return (
+
         <div className='container'>
+        {/* User input search */}
           <form className='input-group'>
              <span className='input-group-text'>0</span>
                 <input type='text' className='form-control' placeholder='Filter by title, company, expertise...'/>
@@ -25,7 +38,28 @@ useEffect(() => {
               </div>
                 
                 <button type='button' className='btn btn-primary'>Search</button>
-          </form>  
+          </form>
+        
+        {jobPost && (
+            <div className="row">
+              {jobPost.map(job => 
+
+              <div className="col-sm-4">
+                <div className='card' style={{width: '18rem', height:'15rem'}}>
+                    <div className="card-body">
+                    <h6 className='card-subtitle'>{job.created_at} - {job.type}</h6>
+                    <h4 className='card-title'>{job.title}</h4>
+                    <h6 className='card-subtitle'>{job.company}</h6>
+
+                 </div>
+                </div>
+
+              </div>     
+            )}  
+            </div>    
+        )}
+
+
         </div>
     )
 }
