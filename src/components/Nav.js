0d9713de80f430c716fca74b0d30ef58e92fbc59
George Flores <georgeflores886@gmail.com>
2020-12-20 13:57:29 -0500

    removed dark theme npm

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+
 import '../styles/Nav.css'
 
 const Nav = () => {
+   
+
     return (
         <>
         <div className='nav-header'>
             <Link to='/' className="brand">devjobs</Link>
+                
 
                 <div className="toggle-theme">
                     <label htmlFor='checkbox'>
